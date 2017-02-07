f4caecb6c8071981c58ee9f11910f72dbd3b883b
343829084 <343829084@qq.com>
2017-02-06 22:50:56 -0800

    add async log file

--- examples/protobuf/rpc/server.cc
+++ examples/protobuf/rpc/server.cc
@@ -1,13 +1,20 @@
 #include <examples/protobuf/rpc/sudoku.pb.h>
 
+
+#include <muduo/base/AsyncLogging.h>
 #include <muduo/base/Logging.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/protorpc/RpcServer.h>
 
 #include <boost/bind.hpp>
-
+#include <stdio.h>
+#include <sys/resource.h>
 using namespace muduo;
 using namespace muduo::net;
+
+int kRollSize = 500*1000*1000;
+
+muduo::AsyncLogging* g_asyncLog = NULL;
 
 namespace sudoku
 {
@@ -29,8 +36,30 @@ class SudokuServiceImpl : public SudokuService
 
 }
 
-int main()
+void asyncOutput(const char* msg, int len)
 {
+  g_asyncLog->append(msg, len);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  {
+    // set max virtual memory to 2GB.
+    size_t kOneGB = 1000*1024*1024;
+    rlimit rl = { 2*kOneGB, 2*kOneGB };
+    setrlimit(RLIMIT_AS, &rl);
+  }
+
+  char name[256];
+  strncpy(name, argv[0], 256);
+  muduo::AsyncLogging log(::basename(name), kRollSize);
+  log.start();
+  g_asyncLog = &log;
+  muduo::Logger::setOutput(asyncOutput);
+
+
   LOG_INFO << "pid = " << getpid();
   EventLoop loop;
   InetAddress listenAddr(9981);
